f6724d53e178057c1cb6919e9338f6d241aa1971
kauanrossii <kauan.rossi@hotmail.com>
2025-06-14 16:29:05 -0300

    feat: generate events, groups, participations and users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GroupsModule } from './groups/groups.module';
+import { EventsModule } from './events/events.module';
+import { ParticipationsModule } from './participations/participations.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [GroupsModule, EventsModule, ParticipationsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
